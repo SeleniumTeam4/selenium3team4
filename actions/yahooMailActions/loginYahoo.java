56d629550ab8774e5b97aad8179e22f39e050413
trietnguyen267 <triet.nguyen267@gmail.com>
2015-06-25 11:17:25 +0700

    sample test

---
+++ actions/yahooMailActions/loginYahoo.java
@@ -0,0 +1,14 @@
+package yahooMailActions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import yahooMail.loginPage;
+
+public class loginYahoo {
+	WebDriver driver;
+	yahooMail.loginPage objLoginPage;	
+	
+	public void enterUserName(String strUserName){
+				
+	}
+}
---
+++ interfaces/yahooMail/loginPage.java
@@ -0,0 +1,11 @@
+package yahooMail;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class loginPage {
+	WebDriver driver;
+	By userNameTextBox = By.id("login-username");
+	By passwordTextBox = By.id("'login-passwd");
+	By signInButton = By.id("login-signin");
+}
